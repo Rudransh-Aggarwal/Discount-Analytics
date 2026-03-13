3e2a2cbf8480de1b85e90fd5ced4845582cf44a0
Rudransh Aggarwal <rudranshaggarwal1507@gmail.com>
2026-03-13 17:35:42 +0530

    trying new things

--- ml-service/build.sh
+++ ml-service/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
 pip install --upgrade pip
-pip install --prefer-binary -r requirements.txt
+pip install --only-binary=:all: numpy==1.23.5 pandas==1.5.3 scikit-learn==1.2.2 joblib==1.2.0
+pip install fastapi==0.111.0 uvicorn==0.29.0 "pydantic==2.7.1"
